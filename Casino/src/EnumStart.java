5294ae53a1dddc9554ccd1a9909d1626e7f2112c
Nbdnus <137500381+Nbdnus@users.noreply.github.com>
2023-06-23 13:59:58 +0200

    Add files via upload

---
+++ Casino/src/EnumStart.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class EnumStart {
+
+
+    public static void main(String[] args) {
+
+
+        for (Wochentag value : Wochentag.values()) {
+
+            System.out.println(value.toString());
+        }
+
+//        int zahl = Integer.parseInt(JOptionPane.showInputDialog("zahl eingeben"));
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
---
+++ Casino/src/Wochentag.java
@@ -0,0 +1,31 @@
+public enum Wochentag {
+
+    MONTAG(1),
+    DIENSTAG(2),
+    MITTWOH(3),
+    DO(4),
+    FREITAG(5),
+    SAMSTAG(6),
+    SONNTAG(7);
+
+    private int position;
+
+    Wochentag(int position) {
+        this.position = position;
+    }
+
+    public boolean istWochenende(){
+
+        if (this == Wochentag.SAMSTAG || this == Wochentag.SONNTAG){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
