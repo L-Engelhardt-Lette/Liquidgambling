54e50fe239a6008c60c0798980e5456853b9a914
n.bodinus <n.bodinus@edu.letteverein.berlin>
2023-06-30 10:11:34 +0200

    Kommentare drin

--- Casino/src/stuff/Currency.java
+++ Casino/src/stuff/Currency.java
@@ -1,13 +1,16 @@
 package stuff;
-
+// Klasse für die beiden Währungen des Users.
+// Pearl = Geldwährung, welche auch durch die games verdient wird.
+// Freespin = 1 mal ohne Pearl zu bezahlen spielen.
 public class Currency {
     private int pearl;
     private int freespin_value;
 
     public Currency() {
-    this.pearl = 1;
-    freespin_value = pearl *100;
+        this.pearl = 1;
+        freespin_value = pearl *100;
 
+        // getter-Methoden für die beiden Währungen.
     }
     public int getFreespin_value(){
         return freespin_value;
