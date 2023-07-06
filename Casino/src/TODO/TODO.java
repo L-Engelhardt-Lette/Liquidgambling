61477e139640a25ac8408756ecf240b062a7a6b1
n.bodinus <n.bodinus@edu.letteverein.berlin>
2023-07-06 12:26:02 +0200

    TODO VON CHRIS DEM ALLER BESTEN NICK STINKT HARD NACH MAGI

---
+++ Casino/src/TODO/TODO.java
@@ -0,0 +1,60 @@
+package TODO;
+
+
+/* TODO:
+Frontend:
+1. Logout und User-Ui fertigstellen
+2. Artifacts fixen, sodass es funtkioniert
+3. Coinflip Game fixen
+   --> 1. Textfeld zum bieten mit Buttons ersetzen( 5 / 10 / 100 / 1000)
+       2. Reset Button, um das gebotene zu reseten
+       3. Kopf und Zahl Buttons zum bieten auf ein bestimmtes Ergebnis
+       4. evtl. Animation von stacken der Coins anstatt Anzeige wie viel man geboten hat
+4. Lucas Mom lieb haben
+5. Alles dokumentieren
+
+Backend:
+1. Coinflip Methoden schreiben für Heads oder Tails
+2. Mütter ficken
+3. Logout mit Funktionen versetzen
+4. Alles dokumentieren --> Kommentare usw.
+5. Lucas Arschloch liebhaben
+
+
+FÜR ALLE !:
+
+PRÄSENTATION VORBEREITEN!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class TODO {
+    private boolean todo;
+    public TODO(boolean todo) {
+        this.todo = todo;
+    }
+
+
+}
